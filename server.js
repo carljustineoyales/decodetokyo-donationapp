3bf3d670d1da22d193f4598bb479af6a68220a7b
Cj Oyales <carljustineoyales@gmail.com>
2020-02-03 10:35:25 +0800

    added alert

--- server.js
+++ server.js
@@ -13,47 +13,6 @@ app.get('*', (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-app.get('/name',(req,res) => {
-  const data = {
-    username:'test',
-    age:5
-  };
-  // res.json(data);
-  res.json('this is working')
-});
-
-app.post('/pay',(req,res) => {
-  console.log('hey');
-  console.log('Body:',req.body);
-  res.json({
-    msg:'request recieved',
-    request:req.body
-  });
-  
-
-  const {first_name,last_name,email,amount,donation_ref,anonymous,campaign} = req.body.props.data;
-
-  const data = {
-    name:first_name + ' ' +last_name,
-    email:email,
-    donation:amount,
-    donation_ref:donation_ref,
-    anonymous:anonymous,
-    campaigns:{
-      id:campaign
-    }
-  }
-  axios.post('https://limitless-brushlands-81295.herokuapp.com/supporters',data
-  )
-  .then(res=>{
-    console.log(data);
-    console.log(res.data);
-  })
-  .catch(err=>{
-    console.log(`Error: ${err.response.data.message}`);
-  })
-
-});
 
 
 app.listen(port, () =>{console.log(`Server Started on port ${port}`)}) 
