cba05d3a3b1495a7699c900dadef9cc2f5c983a0
Cj Oyales <carljustineoyales@gmail.com>
2020-02-07 15:14:43 +0800

    Approve/Decline Checkout Request

--- client/src/components/functions.js
+++ client/src/components/functions.js
@@ -11,5 +11,5 @@ export const getId = () => {
 	return localStorage.getItem('id');
 }
 
-export const strapi = 'http://localhost:1337'; //Change this when deploying
-// export const strapi = 'https://limitless-brushlands-81295.herokuapp.com';
+// export const strapi = 'http://localhost:1337'; //Change this when deploying
+export const strapi = 'https://limitless-brushlands-81295.herokuapp.com';
