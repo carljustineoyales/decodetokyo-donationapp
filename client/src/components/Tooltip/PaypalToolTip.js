766ffb4465dc3263957c6b64bc8b31de8ea423db
Cj Oyales <carljustineoyales@gmail.com>
2020-03-10 13:26:19 +0800

    Tooltip functionality

---
+++ client/src/components/Tooltip/PaypalToolTip.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class PaypalToolTip extends Component {
+  render() {
+    return (
+      <div>
+        Paypal
+      </div>
+    );
+  }
+}
+
+export default PaypalToolTip;
